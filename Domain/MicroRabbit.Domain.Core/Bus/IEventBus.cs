92e86bfaa3c5cebcffb013326cf564d8154474a5
development <evaughan@nrel.gov>
2020-10-09 13:29:05 -0600

    Domain classes and interfaces created

---
+++ Domain/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Events;
+using MicroRabbit.Domain.Core.Commands;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+         Task SendCommand<T>(T command) where T : Command;
+
+         void Publish<T>(T @event) where T : Event;
+
+         void Subscribe<T, THandler>() where T : Event where THandler : IEventHandler<T>;
+    }
+}
---
+++ Domain/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,15 @@
+using MediatR;
+using System;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        public string MessageType { get; protected set; } 
+
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}

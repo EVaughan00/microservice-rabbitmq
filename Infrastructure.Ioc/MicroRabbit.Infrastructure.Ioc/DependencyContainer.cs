c9a47e0f3c2494c2a1ecd7c1b98d1e45cc805454
evaughan <evanvaughan9@gmail.com>
2020-10-12 11:39:21 -0600

    IoC container initialized

---
+++ Infrastructure.Ioc/MicroRabbit.Infrastructure.Ioc/DependencyContainer.cs
@@ -0,0 +1,11 @@
+namespace MicroRabbit.Infrastructure.Ioc
+{
+    public class DependencyContainer
+    {
+        public static voidRegisterServices(IServiceCollection services)
+        {
+            // Doman Bus 
+            services.AddTransient
+        }
+    }
+}
